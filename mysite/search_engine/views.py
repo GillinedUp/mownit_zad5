91dea7a68c7fcdb1f3a1e943523b9d1df8ac605c
GillinedUp <myrreah@gmail.com>
2017-12-23 17:07:17 +0200

    added detail view of article

--- mysite/search_engine/views.py
+++ mysite/search_engine/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
+from django.views import generic
+from .models import Article
 
-# Create your views here.
-
-from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world! You're at the search_engine index.")
+
+
+# class DetailView(generic.DetailView):
+#     model = Article
+#     template_name = 'seach_engine/detail.html'
+
+def detail(request, article_id):
+    article = get_object_or_404(Article, pk=article_id)
+    return render(request, 'search_engine/detail.html', {'article': article})
