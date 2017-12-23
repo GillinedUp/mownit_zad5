e4137e7de6dd8807288f0b66ccbf92e87216f838
GillinedUp <myrreah@gmail.com>
2017-12-23 14:56:44 +0200

    added articles to db

--- mysite/search_engine/models.py
+++ mysite/search_engine/models.py
@@ -12,3 +12,6 @@ class Article(models.Model):
     title = models.CharField(max_length=100)
     text = models.TextField()
     objects = ArticleManager()
+
+    def __str__(self):
+        return self.title
