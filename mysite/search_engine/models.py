acd65719eef684fcf9c9d7ed61ae13e29c9a00cc
GillinedUp <myrreah@gmail.com>
2017-12-23 13:58:47 +0200

    fixed path and filename bugs; added model

--- mysite/search_engine/models.py
+++ mysite/search_engine/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class ArticleManager(models.Manager):
+    def create_article(self, title, text):
+        article = self.create(title=title, text=text)
+        # do something with the book
+        return article
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    objects = ArticleManager()
